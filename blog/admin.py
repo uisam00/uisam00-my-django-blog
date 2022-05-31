6ac4098bfa2a5b99d2a0f28471713ccad8dfbf5e
Ui <uisam.santos@gmail.com>
2022-05-31 14:51:31 -0300

    criacao do modelo de post

---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'publicado', 'status')
+    list_filter = ('status', 'criado', 'publicado')
+    raw_id_fields = ('autor',)
+    date_hierarchy = 'publicado'
+    search_fields = ('titulo', 'conteudo')
+    prepopulated_fields = {'slug': ('titulo',)}
